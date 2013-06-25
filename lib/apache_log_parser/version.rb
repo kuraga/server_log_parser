a9f0629b039f23918e6b0962de35395827c324af
Nick Charlton <hello@nickcharlton.net>
2013-06-25 11:53:37 +0100

    Restructures to fit the Bundler template.
    * Removes most of the docstrings; these will be readded later.
    * Now you must do `require 'apache_log_parser'` as the alias is gone.
    * Version much simplified; it's now SemVer anyway.

--- lib/apache_log_parser/version.rb
+++ lib/apache_log_parser/version.rb
@@ -1,30 +1,3 @@
-#
-# = Apache Log Parser
-#
-# Ruby library to parse web server log files using regular expressions.
-#
-# Category::    
-# Package::     ApacheLogParser
-# Author::      Nick Charlton <hello@nickcharlton.net>
-# License::     MIT License
-#
-#--
-# SVN: $Id$
-#++
-
-
 class ApacheLogParser
-  
-  module Version #:nodoc:
-    MAJOR = 0
-    MINOR = 1
-    TINY  = 0
-    
-    STRING = [MAJOR, MINOR, TINY].join('.')
-  end
-  
-  VERSION         = Version::STRING
-  STATUS          = 'alpha'
-  BUILD           = ''.match(/(\d+)/).to_a.first
-  
+  VERSION = "0.0.1"
 end
