134bc14a9fb5985d4cb7e7b7a48d509bfcb00a38
Kurakin Alexander <kuraga333@mail.ru>
2016-02-16 14:26:30 +0300

    Tests: Add constants test

--- test/web_log_parser_test.rb
+++ test/web_log_parser_test.rb
@@ -7,6 +7,17 @@ describe WebLogParser do
     @parser = WebLogParser.new(@format)
   end
 
+  describe "constants" do
+
+    it "should be correct" do
+      assert_equal(WebLogParser::COMMON_LOG_FORMAT, '%h %l %u %t \"%r\" %>s %b')
+      assert_equal(WebLogParser::COMMON_LOG_FORMAT_VIRTUAL_HOST, '%v %h %l %u %t \"%r\" %>s %b')
+      assert_equal(WebLogParser::COMBINED, '%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"')
+      assert_equal(WebLogParser::COMBINED_VIRTUAL_HOST, '%v %h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"')
+    end
+
+  end
+
   describe "regular expression" do
 
     it "should be correct" do
