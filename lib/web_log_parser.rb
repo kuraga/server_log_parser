9403d1afaf278cf94f5930eda8305a04a49372ef
Kurakin Alexander <kuraga333@mail.ru>
2016-02-16 14:56:07 +0300

    Remove whitespaces

--- lib/web_log_parser.rb
+++ lib/web_log_parser.rb
@@ -1,18 +1,18 @@
 require 'web_log_parser/version'
 
-class WebLogParser  
+class WebLogParser
   COMMON_LOG_FORMAT = '%h %l %u %t \"%r\" %>s %b'
   COMMON_LOG_FORMAT_VIRTUAL_HOST = '%v %h %l %u %t \"%r\" %>s %b'
   COMBINED = '%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"'
   COMBINED_VIRTUAL_HOST = '%v %h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"'
-  
+
   #
   # = ParseError
   #
   # Raised in case the parser can't parse a log line with current +format+.
   #
   class ParseError < RuntimeError; end
-  
+
 
   # Regexp instance used for parsing a log line.
   attr_reader :regexp
@@ -44,18 +44,18 @@ class WebLogParser
 
   # Same as <tt>WebLogParser#parse</tt> but raises a <tt>ParseError</tt>
   # if <tt>line</tt> doesn't match current <tt>format</tt>.
-  # 
+  #
   # ==== Raises
-  # 
+  #
   # ParseError:: if <tt>line</tt> doesn't match current <tt>format</tt>
-  # 
+  #
   def parse!(line)
     parse(line) || raise(ParseError, "Invalid format `%s` for line `%s`" % [@format, line])
   end
-  
-  
+
+
   protected
-    
+
     # Overwrite this method if you want to use some human-readable name for log fields.
     # This method is called only once at <tt>parse_format</tt> time.
     def rename_this_name(name)
@@ -70,14 +70,14 @@ class WebLogParser
       format.gsub!(/[ \t]+/, ' ')  # replace tabulations or spaces with a space
 
       strip_quotes = proc { |string| string.gsub(/^\\"/, '').gsub(/\\"$/, '') }
-      find_quotes  = proc { |string| string =~ /^\\"/ } 
+      find_quotes  = proc { |string| string =~ /^\\"/ }
       find_percent = proc { |string| string =~ /^%.*t$/ }
       find_referrer_or_useragent = proc { |string| string =~ /Referer|User-Agent/ }
-      
+
       pattern = format.split(' ').map do |element|
         has_quotes = !!find_quotes.call(element)
         element = strip_quotes.call(element) if has_quotes
-        
+
         self.names << rename_this_name(element)
 
         case
